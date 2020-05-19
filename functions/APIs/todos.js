2b02923252800cf047033c3c81b821b0a24bf46e
StephanoAH <Stephanoah@protonmail.com>
2020-05-18 23:30:46 -0400

    Add new features
    Add CRUD

--- functions/APIs/todos.js
+++ functions/APIs/todos.js
@@ -1,15 +1,71 @@
+const { db } = require("../util/admin.js");
+
 exports.getAllTodos = (request, response) => {
-  todos = [
-    {
-      id: "1",
-      title: "greeting",
-      body: "Hello world from sharvin shah",
-    },
-    {
-      id: "2",
-      title: "greeting2",
-      body: "Hello2 world2 from sharvin shah",
-    },
-  ];
-  return response.json(todos);
+  db.collection("todos")
+    .orderBy("createdAt", "desc")
+    .get()
+    .then((data) => {
+      let todos = [];
+
+      data.forEach((doc) => {
+        todos.push({
+          todoId: doc.id,
+          title: doc.data().title,
+          body: doc.data().body,
+          createdAt: doc.data().createdAt,
+        });
+      });
+      return response.json(todos);
+    })
+    .catch((err) => {
+      console.error(err);
+      return response.status(500).json({ error: err.code });
+    });
+};
+
+exports.postOneTodo = (request, response) => {
+  if (request.body.body.trim() === "") {
+    return response.status(400).json({ body: "Must not be empty" });
+  }
+
+  if (request.body.title.trim() === "") {
+    return response.status(400).json({ title: "Must not be empty" });
+  }
+
+  const newTodoItem = {
+    title: request.body.title,
+    body: request.body.body,
+    createdAt: new Date().toISOString(),
+  };
+  db.collection("todos")
+    .add(newTodoItem)
+    .then((doc) => {
+      const responseTodoItem = newTodoItem;
+      responseTodoItem.id = doc.id;
+      return response.json(responseTodoItem);
+    })
+    .catch((err) => {
+      console.error(err);
+      return response.status(500).json({ error: "Something went wrong" });
+    });
+};
+
+exports.deleteTodo = (request, response) => {
+  const document = db.doc(`/todos/${request.param.todoId}`);
+
+  document
+    .get()
+    .then((doc) => {
+      if (!doc.exists) {
+        return response.status(400).json({ error: "Todo not found" });
+      }
+      return document.delete();
+    })
+    .then(() => {
+      return response.json({ message: "Delete sucessfull" });
+    })
+    .catch((err) => {
+      console.error(err);
+      return response.status(500).json({ error: err.code });
+    });
 };
